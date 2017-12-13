1660307b63d12aa0c724f7c6d6a14d2b0eceea40
0n11san <Jon@Jons-MacBook-Pro.local>
2017-12-13 08:08:52 -0500

    uploaded files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,25 @@
 var wins = 0;
 var losses = 0;
 var currentSessionTalliedValue = 0;
+var currentGameTargetValue = 0;
 ////////
 var randomTargetValue = [0,1,2,3]; //need 4 (one for each crystal)
 
-for (i=0; i <4; i++){randomTargetValue[i] = Math.floor(Math.random()*12)}
+//declare reset function that runs after game is either won or lost: function resetGame(){}
+function resetNewGame(){
+  for (i=0; i <4; i++){randomTargetValue[i] = Math.floor(Math.random()*12)}
 
-console.log ("red value: " + randomTargetValue[0]);
-console.log ("blue value: " + randomTargetValue[1]);
-console.log ("yellow value: " + randomTargetValue[2]);
-console.log ("green value: " + randomTargetValue[3]);
-/////
-var currentGameTargetValue = Math.floor(Math.random()*(120-19) + 19); //returns a random number in between 19 and 120 per this source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for_each...in
-console.log ("currentGameTargetValue: " + currentGameTargetValue);
-$("#targetValueDisplay").html("TARGET VALUE: " + currentGameTargetValue);
-//make it so that when each button/crystal is clicked (onClick/eventListener), the associated value is added to the user's value for that particualr game session (e.g. currentSessionTalliedValue);
+  console.log ("red value: " + randomTargetValue[0]);
+  console.log ("blue value: " + randomTargetValue[1]);
+  console.log ("yellow value: " + randomTargetValue[2]);
+  console.log ("green value: " + randomTargetValue[3]);
+  /////
+  currentGameTargetValue = Math.floor(Math.random()*(120-19) + 19); //returns a random number in between 19 and 120 per this source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for_each...in
+  console.log ("currentGameTargetValue: " + currentGameTargetValue);
+  $("#targetValueDisplay").html("TARGET VALUE: " + currentGameTargetValue);
+}
+resetNewGame();
+//make it so that when each button/crystal is clicked (onClick/eventListener), the associated value is added to the user's value for that particular game session (e.g. currentSessionTalliedValue);
 //then said value is shown on screen in the "userValueDisplay" div. function defined (but not called yet) here:
 function displayUpdatedValue (){$("#userValueDisplay").html("Your total value is: " + currentSessionTalliedValue);}
 //source for click jQuery found here: https://www.w3schools.com/jquery/event_click.asp
@@ -25,26 +30,42 @@ $("#red").click(function(){
    currentSessionTalliedValue +=randomTargetValue[0];
  console.log("TalliedValue: " + currentSessionTalliedValue);
  displayUpdatedValue ();
+ lossOrWin ();
 });
 $("#blue").click(function(){
    currentSessionTalliedValue +=randomTargetValue[1];
  console.log("TalliedValue: " + currentSessionTalliedValue);
  displayUpdatedValue ();
+ lossOrWin ();
 });
 $("#yellow").click(function(){
    currentSessionTalliedValue +=randomTargetValue[2];
  console.log("TalliedValue: " + currentSessionTalliedValue);
  displayUpdatedValue ();
+ lossOrWin ();
 });
 $("#green").click(function(){
    currentSessionTalliedValue +=randomTargetValue[3];
  console.log("TalliedValue: " + currentSessionTalliedValue);
  displayUpdatedValue ();
+ lossOrWin ();
 });
 //game begins when user clicks one of the buttons/crystals
 
-//declare reset function that runs after game is either won or lost: function resetGame(){}
-
-//describe the conditon  of losing and consequences i.e if user valueTotal > currentGameTargetValue, then reset game and increase losses count by 1 (losses++; resetGame())
-
-//describes the conditions of winning else if (userValueTotal === currentGameTargetValue) {wins++; resetGame()}
+function lossOrWin () {
+  //describe the conditon  of losing and consequences i.e if user valueTotal > currentGameTargetValue, then reset game and increase losses count by 1 (losses++; resetNewGame())
+  if (currentSessionTalliedValue > currentGameTargetValue){
+    losses++;
+    $("#losses").html("Losses: " + losses);
+    resetNewGame();
+    alert ("you lost. sorry number is too big :( ");
+  }
+  //describes the conditions of winning else if (userValueTotal === currentGameTargetValue) {wins++; resetGame()}
+  else if (currentSessionTalliedValue === currentGameTargetValue) {
+    wins++;
+    $("#wins").html("wins: " + wins);
+    resetNewGame();
+    alert ("you won!");
+    }
+}
+//call lossOrWin () function whenever buttons are clicked
